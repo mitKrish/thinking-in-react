5906af82ebc4ce8100611545c229f215ed99c0c8
Krishna Kumar <mitkrishnakumar@gmail.com>
2024-10-07 01:17:15 +0530

    add mern
    add mern

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
+import { useRef } from "react";
+import { useState } from "react";
+
 export default function App() {
-  return <></>;
+  const ref = useRef(0);
+  const [count, setCount] = useState(0);
+
+  function handleRefClick() {
+    ref.current = ref.current + 1;
+    alert("Clicked " + ref.current + " times!");
+  }
+
+  function handleStateClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <>
+      <button type="button" onClick={handleRefClick}>
+        Ref: Clicked {ref.current} times
+      </button>
+      <button type="button" onClick={handleStateClick}>
+        State: Clicked {count} times
+      </button>
+    </>
+  );
 }
